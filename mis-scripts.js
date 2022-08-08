778d0a1618ef94a22afa35f5a966f3e7a39547fc
javierlg91 <javilogut91@gmail.com>
2022-08-08 11:23:25 +0100

    asdad

--- mis-scripts.js
+++ mis-scripts.js
@@ -1,5 +1,4 @@
 const nav = document.getElementById('menu');
-const navButton = document.getElementById('toggle-menu');
 const OFFSET = 150.0;
 const blueButton = document.getElementById('blue-button');
 const marcoIzquierdo = document.querySelector('.marco-izquierdo');
@@ -11,7 +10,7 @@ window.addEventListener('scroll', function () {
   const doc = document.documentElement;
   const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
   if (top > OFFSET) {
-    nav.classList.add('bg-white', 'text-gray-800');
+    nav.classList.add('bg-white');
     nav.classList.remove('translate-y-20', 'text-white');
     marcoIzquierdo.classList.remove('translate-x-6');
     marcoIzquierdo.classList.remove('mt-6', 'mb-6');
@@ -21,8 +20,8 @@ window.addEventListener('scroll', function () {
     navButton.classList.add('text-blue-300');
     mostrar.classList.add('lowercase');
   } else {
-    nav.classList.remove('bg-white', 'text-gray-800');
-    nav.classList.add('translate-y-20', 'text-black');
+    nav.classList.remove('bg-white');
+    nav.classList.add('translate-y-20');
     marcoIzquierdo.classList.add('translate-x-6');
     marcoIzquierdo.classList.add('mt-6', 'mb-6');
     marcoDerecho.classList.add('-translate-x-6');
@@ -36,8 +35,11 @@ window.addEventListener('scroll', function () {
 const menuOpen = document.getElementById('abierto');
 const menuClosed = document.getElementById('cerrado');
 const navResponsive = document.getElementById('nav-responsive');
+const navButton = document.getElementById('toggle-menu');
+
 navButton.addEventListener('click', function () {
   if (navResponsive.classList.contains('hidden')) {
+    console.log('funciona');
     navResponsive.classList.remove('hidden');
     navResponsive.classList.add(
       'flex',
