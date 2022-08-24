ff397bf7642fb24109f83b0136f123e2554be54a
javierlg91 <javilogut91@gmail.com>
2022-08-24 09:28:30 +0100

    ..

--- send.php
+++ send.php
@@ -1,21 +1,12 @@
 <?php
-error_reporting(0);
-$nombre = $_POST['nombre'];
-$correo_electronico= $_POST['email'];
+	$nombre = $_POST['nombre'];
+	$email = $_POST['email'];
+	$asunto = 'Formulario Rellenado';
+	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['mensaje'];
 
-$mensaje=$_POST['mensaje$mensaje'];
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-
-$para = "javilogut91@gmail.com";
-$asunto = "AQUÍ LO QUE QUIERAS";
-
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
-echo 'mensaje enviado correctamente';
+	if(mail('javilogut91@gmail.com', $asunto, $mensaje)){
+		echo "Correo enviado";
+	}
 
 ?>
